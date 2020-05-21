9fae6f0258bbc9c1256ece59d95a1f44ab76b1d6
Jud Taylor <jud.taylor@protonmail.com>
2020-05-21 19:26:30 -0400

    Cookies!

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,26 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+def eating_cookies(n):
+
+    cookie_memo = {}
+
+    # base cases
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    elif n <= 2:
+        return n
+    elif n == 3:
+        return 4
+
+    # # non  base case
+    elif n not in cookie_memo:
+        cookie_memo[n] = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        
+    return cookie_memo[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
