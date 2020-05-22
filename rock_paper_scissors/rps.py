91b9125d16d6b7962cfe3720165f2425774f6d5b
Jud Taylor <jud.taylor@protonmail.com>
2020-05-22 00:25:21 -0400

    Works

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,10 +2,30 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  # Your code here
+def rock_paper_scissors (n):
+    plays = [['rock'], ['paper'], ['scissors']]
+    
+    # can't go back in time!
+    if n < 0:
+        return "Impossible"
+    
+    # Zero plays is boring
+    elif n == 0:
+        return [[]]
+    
+    # Stll boring
+    elif n == 1: 
+        return plays
+    
+    else:
+        ans = []
+        for play in plays:
+            for simpler_part in rock_paper_scissors(n-1):
+                ans.append(play + simpler_part)
+        return ans
 
-  pass
+
+
 
 
 if __name__ == "__main__":
